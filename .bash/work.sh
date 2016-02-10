465ee5f72e26fd4f9546e05f773ca9f94ea0f0a9
Eric Johnson <eric.git@iijo.org>
2016-02-10 08:20:57 -0500

    .

--- .bash/work.sh
+++ .bash/work.sh
@@ -1,3 +1,8 @@
 [ -f ~/.bash_aliases    ] && . ~/.bash_aliases
 [ -f ~/.bash_functions  ] && . ~/.bash_functions
 [ -f ~/.rex-environment ] && . ~/.rex-environment
+
+if [ ! -f ~/.vim/bundle/vim-fugitive/.git ]; then
+    yadm submodule init
+    yadm submodule update
+fi
