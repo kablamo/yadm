7683a49e09ee9622128853cc156db482e5c7f15c
Eric Johnson <eric.git@iijo.org>
2017-11-24 04:38:17 -0500

    .

--- .bash/work.sh
+++ .bash/work.sh
@@ -6,14 +6,9 @@
 if [ ! -f ~/.yadm/first-time ]; then
     cd
     yadm reset --hard origin/master
-    yadm submodule init
-    yadm submodule update
     touch ~/.yadm/first-time
     cd -
     exec bash -l
-else
-    yadm submodule init
-    yadm submodule update
 fi
 
 [ -r $HOME/.dataprinter ] && chmod g-w $HOME/.dataprinter
